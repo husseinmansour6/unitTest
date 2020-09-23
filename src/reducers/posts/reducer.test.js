4d225de89078cb02ed22f9b8994d9c7d86591141
husseinmansour6 <hmansour@aldogroup.com>
2020-09-22 22:22:44 -0400

    posts reducer with tests

---
+++ src/reducers/posts/reducer.test.js
@@ -0,0 +1,17 @@
+import { types } from "../../actions/types"
+import postsReducer from "./reducer"
+
+describe("Posts reducer", () => {
+  it("should return default state", () => {
+    const newState = postsReducer(undefined, {})
+    expect(newState).toEqual([])
+  })
+  it("should return new state if receiving type", () => {
+    const posts = [{ title: "test1" }, { title: "test2" }, { title: "test3" }]
+    const newState = postsReducer(undefined, {
+      type: types.GET_POSTS,
+      payload: posts
+    })
+    expect(newState).toEqual(posts)
+  })
+})
